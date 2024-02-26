cf3b907ecd051ad314a1a3cc0d33249137682006
kirilliusy <jigaloffkir@gmail.com>
2024-02-26 14:46:00 +0300

    pam

--- zhigalov 3/4.py
+++ zhigalov 3/4.py
@@ -1,18 +1,17 @@
 import random
-from random import randint
 def MathForKids():
     a = random.randint(1, 10)
     b = random.randint(1, 10)
     c = int(input(f'{a} + {b} = '))
     return (a + b) == c
 k = 0
-t=0
+t = 0
 while k < 3:
     if not(MathForKids()):
         k += 1
     else:
         t += 1
-print("Игра Ококнчена. Правильных ответов: ", t)
+print("Игра Окончена. Правильных ответов: ", t)
 
 
 
