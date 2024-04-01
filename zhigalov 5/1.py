abff2a1d1b12bae250b9e3183b9eeff384ca42ad
kirilliusy <jigaloffkir@gmail.com>
2024-04-01 15:08:23 +0300

    Pupu

--- zhigalov 5/1.py
+++ zhigalov 5/1.py
@@ -8,3 +8,50 @@ def task1():
         print('Вот исходный список - ', numb)
         print('Вы не угадали число((')
 task1()
+
+import random
+def task2():
+    rndm = [""]*10
+    for i in range(0,10):
+        rndm[i] = random.randrange(1,100)
+    print(rndm)
+    k=0
+    for i in range(0,9):
+        for j in range(i+1,9):
+            if rndm[i] == rndm[j]:
+                k+=1
+    if k>0:
+        print(f"Повторяющихся элементов - {k}")
+    else:
+        print('Нет повторяющихся элементов')
+task2()
+
+def task3():
+    days = ('Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье')
+    hday = int(input("Введите количество желаемых выходных: "))
+    print('Ваши выходные дни: ', days[7-hday:7])
+    print('Ваши рабочие дни: ', days[0:7-hday])
+task3()
+
+def task4():
+    MyGr = ["Жигалов", "Удовкин", "Павлова", "Агеечкина", "Розина", "Федорчук", "Игнатенко", "Пичуева", "Коченков", "Ридер"]
+    DrGr = ["Иванов", "Петров", "Сидоров", "Смирнов", "Даут", "Ходырев", "Зайцев", "Шишикин", "Камаев", "Голев"]
+    print(MyGr)
+    print(DrGr)
+    team = ['']*10
+    for i in range(0,5):
+        team[i] = MyGr[i]
+    for i in range(5, 10):
+        team[i] = DrGr[i]
+    korteam = sorted(tuple(team))
+    print("Ваша команда - ", korteam)
+    print(f"Количество игроков - {len(korteam)}")
+    k = 0
+    if "Иванов" in korteam:
+        for i in range(0,10):
+            if "Иванов" == korteam[i]:
+                k+=1
+        print(f'Иванов встречается - {k} раз')
+    else:
+        print("Иванов не встречается")
+task4()
--- zhigalov 6/1.py
+++ zhigalov 6/1.py
@@ -1,2 +1,24 @@
 def task1():
-    return 1
+    country = {'Россия': 'Москва', 'Украина': 'Киев','Беларусь':'Минск','Эстония':'Таллин', "Германия": "Берлин", "Италия": "Рим" }
+    ctr = input("Введите название страны: ")
+    stl = country.get(ctr)
+    print(stl)
+    print(country.items())
+    print(sorted(country.items()))
+#task1()
+def task2():
+    erud = {
+        1: ['А','В','Е','И','Н','О','Р','С','Т'],
+        2: ['Д', 'К', 'Л', 'М', 'П', 'У'],
+        3: ['Б', 'Г', 'Ё', 'Ь', 'Я'],
+        4: ['Й', 'Ы'],
+        5: ['Ж', 'З', 'Х', 'Ц', 'Ч'],
+        8: ['Ш', 'Э', 'Ю'],
+        10: ['Ф', 'Щ', 'Ъ']}
+    kol = 0
+    word = input("Введите своё слово: ").upper()
+    wordn = list(word)
+    for i in range(len(wordn)):
+        kol += erud.get(wordn[i])
+    print()
+task2()
